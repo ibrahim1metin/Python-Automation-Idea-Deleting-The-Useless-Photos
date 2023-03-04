edd01a5071ddc2f137a897aefa4a424f323847e2
ibrahim1metin <107308009+ibrahim1metin@users.noreply.github.com>
2023-03-04 20:56:18 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,82 +1,50 @@
-import tensorflow as tf
-import numpy as np
-from io import BytesIO
-from google.colab import files
-import os
 from PIL import Image
-import tensorflow_probability as tfp
-upl=files.upload()
-images=[]
-for _,j in upl.items():
-  images.append(np.asarray(Image.open(BytesIO(j))))
-for i in range(len(images)):
-  images[i]=tf.image.resize(images[i],(200,300))
-print(images)
-def corr_sim(a,b):
-  covar=tf.squeeze(tfp.stats.covariance(a,b),axis=2)
-  vara=tfp.stats.variance(a)
-  varb=tfp.stats.variance(b)
-  return 1-tf.reduce_mean(tf.squeeze((covar/(tf.sqrt(vara)*tf.sqrt(varb)))))
-def outlier(args):
-    args=sorted(args)
-    outlist=[]
-    def median(args1):
-        if(len(args1)%2==1):
-            return list(sorted(args1))[int((len(args1)/2)-0.5)]
-        else:
-            return (list(sorted(args1))[int(len(args1)/2)]+list(sorted(args1))[int(len(args1)/2)-1])/2
-    def minmax(data):
-        if len(data) < 2: 
-            raise ValueError("Data must contain at least two values to have min and max values.")
-        srt = sorted(data)
-        return srt[0],srt[-1]
-    min,max=minmax(args)
-    if len(args)%2==1:
-        q1=median(args[args.index(min):args.index(median(args)):])
-        q3=median(args[args.index(median(args)):args.index(max):])
-        for i in args:
-            if(i<(q1-1.5*(q3-q1)) or i>(q3+1.5*(q3-q1))):
-                outlist.append(i)
-        return outlist
-    args.append(median(args))
-    args=sorted(args)
-    q1=median(args[args.index(min):args.index(median(args)):])
-    q3=median(args[args.index(median(args)):args.index(max):])
-    for i in args:
-        if(i<(q1-1.5*(q3-q1)) or i>(q3+1.5*(q3-q1))):
-             outlist.append(i)
-    return outlist
-def comparer(image_a,image_b):
-  data=list()
-  for i in range(image_a.shape[1]):
-    for j in range(image_a.shape[-1]):
-      data.append(corr_sim(image_a[::,i:i+1:,j:j+1:],image_b[::,i:i+1:,j:j+1:]))
-  mean=0
-  outli=outlier(data)
-  for i in data:
-    if not tf.math.is_inf(i) and not i in outli:
-      mean+=i
-  mean=mean/len(data)
-  return mean
-threshold=0.80
-def find(arr,it):
-  inx=0
-  for i in arr:
-    if np.all(i==it):
-      break
-    inx+=1
-  return inx
-for i in images:
-  if find(images,i)==len(images)-1:
-    if comparer(images[0],images[-1])<threshold:
-      images.pop(images.index(i))
-    else:
-      continue
-  else:
-    print(comparer(i,images[find(images,i)+1]))
-    if comparer(i,images[find(images,i)+1])<threshold:
-      images.pop(find(images,i))
-      print(len(images))
-    else:
-      continue
-print(images)
+import os
+import numpy as np
+import warnings
+from skimage.metrics import structural_similarity
+import json
+warnings.simplefilter("ignore")
+datas=[i for i in os.listdir(r"data")]
+im_asarray=[]
+files=dict()
+for j in datas: 
+    im=Image.open(f"data/{j}")
+    im=im.resize((200,200))
+    im=np.asarray(im)
+    im_asarray.append(im)
+def standartize(image):
+    std=np.std(image)
+    mean=np.mean(image) 
+    image=(image-mean)/std
+    return image
+def find(element,array,helper=[]):
+    for ind in range(len(array)):
+        if  np.all(array[ind]==element) and ind not in helper: 
+            return ind
+    return None
+def create_couples(list):
+    results=[]
+    for i in range(len(list)):
+        for j in range(i+1,len(list)):
+            results.append((list[i],list[j])) 
+    return results
+processed=list(map(standartize,im_asarray))
+processed2=create_couples(processed)
+print(create_couples(datas))
+useless=[]
+for couple in processed2:
+    sim=structural_similarity(couple[0],couple[1],multichannel=True)
+    if sim>0.6:
+        useless.append(couple)
+useless_to_delete=[i[1] for i in useless]
+indexes=[]
+for i in useless_to_delete:
+    indexes.append(find(i,processed,indexes))
+indexes=[i for i in indexes if i is not None]
+names=[os.path.dirname(os.path.realpath(__file__))+fr"\data\{datas[i]}" for i in indexes]
+as_numpy_arrays=[im_asarray[i] for i in indexes]
+as_PIL=[Image.open(i) for i in names]
+result={"names_and_pathes":names}
+with open("jsonfile.json","w") as file:
+    json.dump(result,file)
